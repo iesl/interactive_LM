7c0f6bcf90e9ae20357a87b960e2a05b093b5c16
Haw-Shiuan Chang <hschang@blake2.cs.umass.edu>
2021-02-07 17:20:36 -0500

    before merge

--- bin/preprocessing_single_proc.sh
+++ bin/preprocessing_single_proc.sh
@@ -11,6 +11,9 @@ OUTPUT_SPACY_DIR="./data/raw/wiki2016_both_small/"
 OUTPUT_GPT2_DIR="./data/raw/wiki2016_gpt2_order_small/"
 OUTPUT_GPT2="./data/raw/wiki2016_gpt2_small.txt"
 OUTPUT_GPT2_PROC_DIR="./data/processed/wiki2016_gpt2_small/"
+OUTPUT_GPT2_TENSOR_DIR="$OUTPUT_GPT2_PROC_DIR/tensors_all_min100/"
+OUTPUT_DICT_FILE="$OUTPUT_GPT2_TENSOR_DIR/dict_idx_compact"
+STOP_WORD_LIST="./resources/stop_word_list"
 
 mkdir -p $OUTPUT_SPACY_DIR
 mkdir -p $OUTPUT_GPT2_DIR
@@ -28,4 +31,4 @@ done
 
 cat $OUTPUT_GPT2_DIR/* > $OUTPUT_GPT2
 eval $PY_PATH src/preprocessing/map_tokens_to_indices_gpt2.py --data $OUTPUT_GPT2 --save $OUTPUT_GPT2_PROC_DIR #Convert the output of Spacy tokenizer to word index
-#Align tokens from Spacy and from GPT2
+eval $PY_PATH src/preprocessing/map_indices_to_tensors_gpt2_memory_saving.py --data $OUTPUT_GPT2_PROC_DIR --save $OUTPUT_GPT2_TENSOR_DIR --new_dict_file $OUTPUT_DICT_FILE --stop_word_file $STOP_WORD_LIST #Align tokens from Spacy and from GPT2
